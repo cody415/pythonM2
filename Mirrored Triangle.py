7f3484c47d8ee0223c3eeb613ffea7fec36c1e35
cody415 <aparnadesai65@gmail.com>
2026-06-23 11:56:28 +0530

    Implement mirrored right-angled triangle printer
    Add a program to print a mirrored right-angled triangle.

---
+++ Mirrored Triangle.py
@@ -0,0 +1,9 @@
+# Program to print a mirrored right-angled triangle
+
+rows = int(input("Enter the number of rows: "))
+
+for i in range(1, rows + 1):
+    # Print spaces first
+    print(" " * (rows - i), end="")
+    # Then print stars
+    print("*" * i)
