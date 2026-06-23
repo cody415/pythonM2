1739686e3186550579fa31f45dd7bf4ce0036b0b
cody415 <aparnadesai65@gmail.com>
2026-06-23 11:41:00 +0530

    Add age checking program for range 10 to 20

---
+++ check the age.py
@@ -0,0 +1,9 @@
+#Write a program to check the age entered by the user is between 10 to 20 years or not?
+# Program to check if age is between 10 and 20
+
+age = int(input("Enter your age: "))
+
+if 10 <= age <= 20:
+    print("Your age is between 10 and 20 years.")
+else:
+    print("Your age is NOT between 10 and 20 years.")
